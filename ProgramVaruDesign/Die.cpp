3000050ddd7d204a62040a543f42225f47d46110
hasssan4 <hasssan4@hotmail.com>
2014-05-26 19:31:01 +0200

    Implemented getface, rolldie and the constructor

--- ProgramVaruDesign/Die.cpp
+++ ProgramVaruDesign/Die.cpp
@@ -1,11 +1,25 @@
 #include "Die.h"
-
+#include<ctime>
+#include <stdlib.h>
 
 Die::Die()
 {
+	this->face = -1;
+	this->nrOfFaces = 6;
 }
 
+int Die::getFace()const
+{
+	return this->face;
+}
 
+int Die::rollDie()
+{
+	srand(unsigned(time(0)));    // ska finnas i mainWindow.cpp kanske istället
+	
+	this->face = rand() % 6 + 1;
+	return this->face;
+}
 Die::~Die()
 {
 }
